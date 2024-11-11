0380825a685bb8f0ddf4cab6763003cbb5ab792b
Denis <Denis.kerstens@gmail.com>
2024-11-11 20:26:42 +0300

    Added custom item

--- src/main/java/org/test/myfirstmod/item/ModItems.java
+++ src/main/java/org/test/myfirstmod/item/ModItems.java
@@ -6,12 +6,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.test.myfirstmod.Myfirstmod;
+import org.test.myfirstmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =  DeferredRegister.create(ForgeRegistries.ITEMS, Myfirstmod.MOD_ID);
 
     public static final RegistryObject<Item> RANDOMTHINGIMADEINASEPRITEFORTHISMOD = ITEMS.register("randomthingimadeinasepriteforthismod", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> NEW_RANDOM_THING = ITEMS.register("new_random_thing", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetalDetectorItem(new Item.Properties().durability(30)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
