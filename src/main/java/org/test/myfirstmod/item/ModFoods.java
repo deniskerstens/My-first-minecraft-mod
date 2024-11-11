fa7701ea37dc39962eb5e2b6ba0d1162aa6481ed
Denis <Denis.kerstens@gmail.com>
2024-11-11 22:09:33 +0300

    ModFoods.java has been written

--- src/main/java/org/test/myfirstmod/item/ModFoods.java
+++ src/main/java/org/test/myfirstmod/item/ModFoods.java
@@ -1,4 +1,10 @@
 package org.test.myfirstmod.item;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
 public class ModFoods {
+    public static final FoodProperties TEST_FOOD =
+            new FoodProperties.Builder().nutrition(2).saturationMod(0.2f).build();
 }
