53412d2f2029a170aa59a29a343142da261e0e0b
Denis <Denis.kerstens@gmail.com>
2024-11-12 10:20:36 +0300

    Created class
    - Created fuel item FuelItem.java without code(for now)

---
+++ src/main/java/org/test/myfirstmod/block/custom/FuelItem.java
@@ -0,0 +1,11 @@
+package org.test.myfirstmod.block.custom;
+
+import net.minecraft.world.item.Item;
+
+public class FuelItem extends Item {
+    public FuelItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+
+}
